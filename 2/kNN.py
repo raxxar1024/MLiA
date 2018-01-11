10dd2eeb4cd011c28974aca8708446adc0ea8a95
yjw013809 <yjw013809@gmail.com>
2018-01-11 22:48:41 +0800

    kNN

--- 2/kNN.py
+++ 2/kNN.py
@@ -13,8 +13,8 @@ def classify0(inX, dataSet, labels, k):
     diffMat = tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat ** 2
     sqDistances = sqDiffMat.sum(axis=1)
-    distance = sqDistances ** 0.5
-    sortedDistIndicies = distance.argsort()
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
     classCount = {}
     for i in xrange(k):
         voteIlabel = labels[sortedDistIndicies[i]]
@@ -23,6 +23,31 @@ def classify0(inX, dataSet, labels, k):
     return sortedClassCount[0][0]
 
 
+def file2matrix(filename):
+    fr = open(filename)
+    arrayOlines = fr.readlines()
+    numberOfLines = len(arrayOlines)
+    returnMat = zeros((numberOfLines, 3))
+    classLabelVector = []
+
+    index = 0
+    for line in arrayOlines:
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat, classLabelVector
+
+
 if __name__ == "__main__":
-    group, labels = createDataSet()
-    classify0([0, 0], group, labels, 3)
+    import matplotlib
+    import matplotlib.pyplot as plt
+    # group, labels = createDataSet()
+    # classify0([0, 0], group, labels, 3)
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2])
+    plt.show()
