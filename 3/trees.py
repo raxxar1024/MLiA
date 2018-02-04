dd922d2d16437ce164557e79594df906ddbc30b6
yjw013809 <yjw013809@gmail.com>
2018-02-04 23:04:49 +0800

    tree->splitDataSet

--- 3/trees.py
+++ 3/trees.py
@@ -31,6 +31,38 @@ def createDataSet():
     return dataSet, labels
 
 
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeature = len(dataSet[0]) - 1
+    baseEntropy = clacShannonEnt(dataSet)
+    bestInfoGain, bestFeature = 0.0, -1
+    for i in range(numFeature):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy += prob * clacShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
 if __name__ == "__main__":
     myDat, labels = createDataSet()
-    print clacShannonEnt(myDat)
+    # print clacShannonEnt(myDat)
+    # print splitDataSet(myDat, 0, 1)
+    # print splitDataSet(myDat, 0, 0)
+    print chooseBestFeatureToSplit(myDat)
