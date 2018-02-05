2110e27f87aa2dcddd045db7e7652c064094a000
raxxar1024 <yjw013809@gmail.com>
2018-02-05 10:17:29 +0800

    决策树

--- 3/trees.py
+++ 3/trees.py
@@ -66,6 +66,7 @@ def majorityCnt(classList):
     for vote in classList:
         if vote not in classCount.keys():
             classCount[vote] = 0
+        classCount[vote] += 1
     sortedClassCount = sorted(classCount.iteritems(),
                               key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
