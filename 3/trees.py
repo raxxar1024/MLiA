1f69bfe563b5f68f64536889ee787f364b11dcc3
raxxar1024 <yjw013809@gmail.com>
2018-02-05 10:00:32 +0800

    决策树---createTree

--- 3/trees.py
+++ 3/trees.py
@@ -1,4 +1,5 @@
 from math import log
+import operator
 
 
 def clacShannonEnt(dataSet):
@@ -60,9 +61,40 @@ def chooseBestFeatureToSplit(dataSet):
     return bestFeature
 
 
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+    return myTree
+
+
 if __name__ == "__main__":
     myDat, labels = createDataSet()
     # print clacShannonEnt(myDat)
     # print splitDataSet(myDat, 0, 1)
     # print splitDataSet(myDat, 0, 0)
-    print chooseBestFeatureToSplit(myDat)
+    # print chooseBestFeatureToSplit(myDat)
+    myTree = createTree(myDat, labels)
+    print myTree
