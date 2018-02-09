adf50fb98f3015bbbb2715d1cb0c4d1ca51e75f2
raxxar1024 <yjw013809@gmail.com>
2018-02-09 15:39:51 +0800

    bayes

--- 4/bayes.py
+++ 4/bayes.py
@@ -1,3 +1,8 @@
+# !/usr/bin/python
+# -*- coding=utf-8 -*-
+from numpy import *
+
+
 def loadDataSet():
     postingList = [
         ['my', 'dog', 'has', 'flea', 'problem', 'help', 'please'],
@@ -28,9 +33,40 @@ def setOfWords2Vec(vocabList, inputSet):
     return returnVec
 
 
+def trainNB0(trainMatrix, trainCategory):
+    numTrainDocs = len(trainMatrix)
+    numWords = len(trainMatrix[0])
+    pAbusive = sum(trainCategory) / float(numTrainDocs)
+
+    # 初始化概率
+    p0Num = zeros(numWords)
+    p1Num = zeros(numWords)
+    p0Denom = 0.0
+    p1Denom = 0.0
+    for i in range(numTrainDocs):
+        if trainCategory[i] == 1:
+            p1Num += trainMatrix[i]
+            p1Denom += sum(trainMatrix[i])
+        else:
+            p0Num += trainMatrix[i]
+            p0Denom += sum(trainMatrix[i])
+    p1Vec = p1Num / p1Denom
+    p0Vec = p0Num / p0Denom
+    return p0Vec, p1Vec, pAbusive
+
+
 if __name__ == "__main__":
     listOPosts, listClasses = loadDataSet()
+    # myVocabList = createVocabList(listOPosts)
+    # print myVocabList
+    # print setOfWords2Vec(myVocabList, listOPosts[0])
+    # print setOfWords2Vec(myVocabList, listOPosts[1])
+
     myVocabList = createVocabList(listOPosts)
-    print myVocabList
-    print setOfWords2Vec(myVocabList, listOPosts[0])
-    print setOfWords2Vec(myVocabList, listOPosts[1])
+    trainMat = []
+    for postinDoc in listOPosts:
+        trainMat.append(setOfWords2Vec(myVocabList, postinDoc))
+    p0V, p1V, pAb = trainNB0(trainMat, listClasses)
+    print p0V
+    print p1V
+    print pAb
