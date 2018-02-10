820b107865bf30be1714d54587a9ef544c193398
raxxar1024 <yjw013809@gmail.com>
2018-02-10 14:40:20 +0800

    bayes

--- 4/bayes.py
+++ 4/bayes.py
@@ -89,8 +89,52 @@ def testingNB():
     print testEntry, 'classified as: ', classifyNB(thisDoc, p0V, p1V, pAb)
 
 
+def textParse(bigString):
+    import re
+    listOfTokens = re.split(r'\\W*', bigString)
+    return [tok.lower() for tok in listOfTokens if len(tok) > 0]
+
+
+def spamTest():
+    docList = []
+    classList = []
+    fullText = []
+    for i in range(1, 26):
+        wordList = textParse(open('email/spam/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1)
+        wordList = textParse(open('email/ham/%d.txt' % i).read())
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0)
+
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet = []
+    for i in range(10):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del (trainingSet[randIndex])
+
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(setOfWords2Vec(vocabList, docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), array(trainClasses))
+    errorCount = 0
+
+    for docIndex in testSet:
+        wordVector = setOfWords2Vec(vocabList, docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) != classList[docIndex]:
+            errorCount += 1
+
+    print 'the error rate is: ', float(errorCount) / len(testSet)
+
+
 if __name__ == "__main__":
-    listOPosts, listClasses = loadDataSet()
+    # listOPosts, listClasses = loadDataSet()
     # myVocabList = createVocabList(listOPosts)
     # print myVocabList
     # print setOfWords2Vec(myVocabList, listOPosts[0])
@@ -105,4 +149,6 @@ if __name__ == "__main__":
     # print p1V
     # print pAb
 
-    testingNB()
+    # testingNB()
+
+    spamTest()
