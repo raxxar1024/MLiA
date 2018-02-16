5158f2fa01d19b219c02028ec1faa0834a2ae0c6
yjw013809 <yjw013809@gmail.com>
2018-02-16 22:29:59 +0800

    logRegres

--- 5/logRegres.py
+++ 5/logRegres.py
@@ -68,11 +68,26 @@ def stocGradAscent0(dataMatrix, classLabels):
     return weights
 
 
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex] * weights))
+            error = classLabels[randIndex] - h
+            weights += alpha * error * dataMatrix[randIndex]
+    return weights
+
+
 if __name__ == "__main__":
     dataArr, labelMat = loadDataSet()
 
     # weights = gradAscent(dataArr, labelMat)
     # plotBestFit(weights.getA())
 
-    weights = stocGradAscent0(array(dataArr), labelMat)
+    # weights = stocGradAscent0(array(dataArr), labelMat)
+    weights = stocGradAscent1(array(dataArr), labelMat)
     plotBestFit(weights)
