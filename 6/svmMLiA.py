f6bdb5f44d6498aa1bbf74f7bcd16f99ff64465e
yjw013809 <yjw013809@gmail.com>
2018-02-23 21:11:09 +0800

    svmMLiA

---
+++ 6/svmMLiA.py
@@ -0,0 +1,18 @@
+# !/usr/bin/python
+# -*- coding=utf-8 -*-
+from math import *
+from numpy import *
+
+
+def loadDataSet(filename):
+    dataMat, labelMat = [], []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+if __name__ == "__main__":
+    print 1
