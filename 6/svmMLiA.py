fede4c87d47fe64e3c2a47f38fdecc04bf978321
yjw013809 <yjw013809@gmail.com>
2018-03-05 22:03:53 +0800

    svmMLiA

--- 6/svmMLiA.py
+++ 6/svmMLiA.py
@@ -99,6 +99,12 @@ class optStruct:
         self.eCache = mat(zeros((self.m, 2)))  # 误差缓存
 
 
+def clacEk(oS, k):
+    fXk = float(multiply(oS.alphas, oS.labelMat).T * (oS.X * oS.X[k, :].T) + oS.b)
+    Ek = fXk - float(oS.labelMat[k])
+    return Ek
+
+
 if __name__ == "__main__":
     dataArr, labelArr = loadDataSet('testSet.txt')
     b, alphas = smoSimple(dataArr, labelArr, 0.6, 0.001, 40)
