1082cf528e06d2f9bc95c6eb7d002fcac88ca21b
yjw013809 <yjw013809@gmail.com>
2018-04-21 21:26:56 +0800

    adaboost

--- 7/adaboost.py
+++ 7/adaboost.py
@@ -14,6 +14,45 @@ def loadSimpData():
     return datMat, classLabels
 
 
+def stumpClassify(dataMatrix, dimen, threshVal, threshIneq):
+    retArray = ones((shape(dataMatrix[0], 1)))
+    if threshIneq == "lt":
+        retArray[dataMatrix[:, dimen] <= threshVal] = -1.0
+    else:
+        retArray[dataMatrix[:, dimen] > threshVal] = 1.0
+    return retArray
+
+
+def buildStump(dataArr, classLabels, D):
+    dataMatrix = mat(dataArr)
+    labelMat = mat(classLabels).T
+    m, n = shape(dataMatrix)
+    numSteps = 10.0
+    bestStump = {}
+    bestClasEst = mat(zeros((m, 1)))
+    minError = inf
+    for i in range(n):
+        rangeMin = dataMatrix[:, i].min()
+        rangeMax = dataMatrix[:, i].max()
+        stepSize = (rangeMax - rangeMin) / numSteps
+        for j in range(-1, int(numSteps) + 1):
+            for inequal in ["lt", "gt"]:
+                threshVal = (rangeMin + float(j) * stepSize)
+                predictedVals = stumpClassify(dataMatrix, i, threshVal, inequal)
+                errArr = mat(ones((m, 1)))
+                errArr[predictedVals == labelMat] = 0
+                weightedError = D.T * errArr
+                # 计算加权错误率
+                print "split: dim %d, thresh %.2f, thresh ineqal: %s, thr weighted error is %.3f" % (
+                i, threshVal, inequal, weightedError)
+                if weightedError < minError:
+                    minError = weightedError
+                    bestClasEst = predictedVals.copy()
+                    bestStump["dim"] = i
+                    bestStump["thresh"] = threshVal
+                    bestStump["ineq"] = inequal
+    return bestStump, minError, bestClasEst
+
+
 if __name__ == "__main__":
     datMat, classLabels = loadSimpData()
-
