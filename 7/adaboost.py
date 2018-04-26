3144700343cc509f1f0c468a8960f803dfa03138
yjw013809 <yjw013809@gmail.com>
2018-04-26 17:39:30 +0800

    adaboost

--- 7/adaboost.py
+++ 7/adaboost.py
@@ -54,10 +54,34 @@ def buildStump(dataArr, classLabels, D):
     return bestStump, minError, bestClasEst
 
 
+def adaBoostTrainDS(dataArr, classLabels, numIt=40):
+    weakClassArr = []
+    m = shape(dataArr)[0]
+    D = mat(ones((m, 1)) / m)
+    aggClassEst = mat(zeros((m, 1)))
+    for i in range(numIt):
+        bestStump, error, classEst = buildStump(dataArr, classLabels, D)
+        print "D:", D.T
+        alpha = float(0.5 * log((1.0 - error) / max(error, 1e-16)))
+        bestStump['alpha'] = alpha
+        weakClassArr.append(bestStump)
+        print "classEst: ", classEst.T
+        # （以下两行）为下一次迭代计算*D*
+        expon = multiply(-1 * alpha * mat(classLabels).T, classEst)
+        D = multiply(D, exp(expon))
+        D = D / D.sum()
+        # （以下五行）错误率累加计算
+        aggClassEst += alpha * classEst
+        print "aggClassEst: ", aggClassEst.T
+        aggErrors = multiply(sign(aggClassEst) != mat(classLabels).T, ones((m, 1)))
+        errRate = aggErrors.sum() / m
+        print "total error: ", errRate, "\n"
+        if errRate == 0.0:
+            break
+    return weakClassArr
+
+
 if __name__ == "__main__":
     datMat, classLabels = loadSimpData()
     D = mat(ones((5, 1)) / 5)
     buildStump(datMat, classLabels, D)
- 
-
- 
