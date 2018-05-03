8ac965247a728e00253be3369c0d150daec03d69
raxxar1024 <yjw013809@gmail.com>
2018-05-03 20:17:34 +0800

    adaboost

--- 7/adaboost.py
+++ 7/adaboost.py
@@ -100,9 +100,9 @@ if __name__ == "__main__":
     D = mat(ones((5, 1)) / 5)
     buildStump(datMat, classLabels, D)
 
-    # classifierArray = adaBoostTrainDS(datMat, classLabels, 9)
-    # print classifierArray
+    classifierArray = adaBoostTrainDS(datMat, classLabels, 9)
+    print classifierArray
 
-    datArr, labelArr = loadSimpData()
-    classifierArr = adaBoostTrainDS(datMat, labelArr, 30)
-    print adaClassify([0, 0], classifierArr)
+    # datArr, labelArr = loadSimpData()
+    # classifierArr = adaBoostTrainDS(datMat, labelArr, 30)
+    # print adaClassify([0, 0], classifierArr)
