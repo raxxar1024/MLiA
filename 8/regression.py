b1d95cd1a189eb1caa1e794756d8bffe6d84093b
yjw013809 <yjw013809@gmail.com>
2018-05-12 23:16:29 +0800

    regression

--- 8/regression.py
+++ 8/regression.py
@@ -11,11 +11,36 @@ def loadDataSet(fileName):
         lineArr = []
         curLine = line.strip().split('\t')
         for i in range(numFeat):
-            lineArr = append(float(curLine[i]))
+            lineArr.append(float(curLine[i]))
         dataMat.append(lineArr)
         labelMat.append(float(curLine[-1]))
     return dataMat, labelMat
 
 
+def standRegres(xArr, yArr):
+    xMat = mat(xArr)
+    yMat = mat(yArr).T
+    xTx = xMat.T * xMat
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I * (xMat.T * yMat)
+    return ws
+
+
 if __name__ == "__main__":
-    a = 1
+    xArr, yArr = loadDataSet('ex0.txt')
+    ws = standRegres(xArr, yArr)
+    xMat, yMat = mat(xArr), mat(yArr)
+    yHat = xMat * ws
+
+    import matplotlib.pyplot as plt
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy.ws
+    ax.plot(xCopy[:, 1], yHat)
