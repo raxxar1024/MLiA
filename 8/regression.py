578dc8ebf1c2b5d7b8bb0b26a6def901756efb9d
yjw013809 <yjw013809@gmail.com>
2018-05-14 22:37:25 +0800

    regression

--- 8/regression.py
+++ 8/regression.py
@@ -1,6 +1,7 @@
 # !/usr/bin/python
 # -*- coding=utf-8 -*-
 from numpy import *
+import matplotlib.pyplot as plt
 
 
 def loadDataSet(fileName):
@@ -28,21 +29,57 @@ def standRegres(xArr, yArr):
     return ws
 
 
+def lwlr(testPoint, xArr, yArr, k=1.0):
+    xMat, yMat = mat(xArr), mat(yArr).T
+    m = shape(xMat)[0]
+    weights = mat(eye((m)))
+    # 创建对角矩阵
+    for j in range(m):
+        # 权重值大小以指数级衰减
+        diffMat = testPoint - xMat[j, :]
+        weights[j, j] = exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+    xTx = xMat.T * (weights * xMat)
+    if linalg.det(xTx) == 0.0:
+        print "This matrix is singular, cannot do inverse"
+        return
+    ws = xTx.I * (xMat.T * (weights * yMat))
+    return testPoint * ws
+
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+    m = shape(testArr)[0]
+    yHat = zeros(m)
+    for i in range(m):
+        yHat[i] = lwlr(testArr[i], xArr, yArr, k)
+    return yHat
+
+
 if __name__ == "__main__":
     xArr, yArr = loadDataSet('ex0.txt')
-    ws = standRegres(xArr, yArr)
-    xMat, yMat = mat(xArr), mat(yArr)
-    yHat = xMat * ws
+    # ws = standRegres(xArr, yArr)
+    # xMat, yMat = mat(xArr), mat(yArr)
+    # yHat = xMat * ws
+    #
+    # fig = plt.figure()
+    # print "break here in case no tk ..."
+    # ax = fig.add_subplot(111)
+    # ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    #
+    # xCopy = xMat.copy()
+    # xCopy.sort(0)
+    # yHat = xCopy * ws
+    # ax.plot(xCopy[:, 1], yHat)
+    # print "wait..."
 
-    import matplotlib.pyplot as plt
+    lwlr(xArr[0], xArr, yArr, 1.0)
+    lwlr(xArr[0], xArr, yArr, 0.001)
+    yHat = lwlrTest(xArr, xArr, yArr, 0.003)
 
+    xMat = mat(xArr)
+    srtInd = xMat[:, 1].argsort(0)
+    xSort = xMat[srtInd][:, 0, :]
     fig = plt.figure()
-    print "break here in case no tk ..."
     ax = fig.add_subplot(111)
-    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
-
-    xCopy = xMat.copy()
-    xCopy.sort(0)
-    yHat = xCopy * ws
-    ax.plot(xCopy[:, 1], yHat)
-    print "wait..."
+    ax.plot(xSort[:, 1], yHat[srtInd])
+    ax.scatter(xMat[:, 1].flatten().A[0], mat(yArr).T.flatten().A[0], s=2, c="red")
+    plt.show()
