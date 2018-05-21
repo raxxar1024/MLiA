d01b8ae85dfbc18e8f4e8c2bf7d077959f21db3c
raxxar1024 <yjw013809@gmail.com>
2018-05-21 08:53:23 +0800

    regression

--- 8/regression.py
+++ 8/regression.py
@@ -84,6 +84,41 @@ def ridgeTest(xArr, yArr):
     return wMat
 
 
+def regularize(xMat):  # regularize by columns
+    inMat = xMat.copy()
+    inMeans = mean(inMat, 0)  # calc mean then subtract it off
+    inVar = var(inMat, 0)  # calc variance of Xi then divide by it
+    inMat = (inMat - inMeans) / inVar
+    return inMat
+
+
+def stageWise(xArr, yArr, eps=0.01, numIt=100):
+    xMat, yMat = mat(xArr), mat(yArr).T
+    yMean = mean(yMat, 0)
+    yMat = yMat - yMean
+    xMat = regularize(xMat)
+    m, n = shape(xMat)
+    returnMat = zeros((numIt, 0))
+    ws = zeros((n, 1))
+    wsTest = ws.copy()
+    wsMax = ws.copy()
+    for i in range(numIt):
+        print ws.T
+        lowestError = inf
+        for j in range(n):
+            for sign in [-1, 1]:
+                wsTest = ws.copy()
+                wsTest[j] += eps * sign
+                yTest = xMat * wsTest
+                rssE = rssError(yMat.A, yTest.A)
+                if rssE < lowestError:
+                    lowestError = rssE
+                    wsMax = wsTest
+            ws = wsMax.copy()
+            returnMat[i, :] = ws.T
+    return returnMat
+
+
 if __name__ == "__main__":
     # xArr, yArr = loadDataSet('ex0.txt')
     # ws = standRegres(xArr, yArr)
