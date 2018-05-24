50bf4d9c9cfda581bee7d5fbb251c9ff328a4c7f
raxxar1024 <yjw013809@gmail.com>
2018-05-24 08:59:44 +0800

    regression

--- 8/regression.py
+++ 8/regression.py
@@ -119,6 +119,45 @@ def stageWise(xArr, yArr, eps=0.01, numIt=100):
         # return returnMat
 
 
+from time import sleep
+import json
+import urllib2
+
+
+def searchForSet(retX, retY, setNum, yr, numPce, origPrc):
+    sleep(10)
+    # myAPIstr = 'get from google.com'
+    searchURL = "lego/lego%d.html" % setNum
+    pg = urllib2.urlopen(searchURL)
+    retDict = json.loads(pg.read())
+    for i in range(len(retDict['items'])):
+        try:
+            currItem = retDict['items'][i]
+            if currItem['product']['condition'] == 'new':
+                newFlag = 1
+            else:
+                newFlag = 0
+            listOfInv = currItem['product']['inventories']
+            for item in listOfInv:
+                sellingPrice = item['price']
+                if sellingPrice > origPrc * 0.5:
+                    # 过滤掉不完整的套装
+                    print "%d\t%d\t%d\t%f\t%f" % (yr, numPce, newFlag, origPrc, sellingPrice)
+                    retX.append([yr, numPce, newFlag, origPrc])
+                    retY.append(sellingPrice)
+        except:
+            print "problem with item %d" % i
+
+
+def setDataCollect(retX, retY):
+    searchForSet(retX, retY, 8288, 2006, 800, 49.99)
+    searchForSet(retX, retY, 10030, 2002, 3096, 269.99)
+    searchForSet(retX, retY, 10179, 2007, 5195, 499.99)
+    searchForSet(retX, retY, 10181, 2007, 3428, 199.99)
+    searchForSet(retX, retY, 10189, 2008, 5922, 299.99)
+    searchForSet(retX, retY, 10196, 2009, 3263, 249.99)
+
+
 if __name__ == "__main__":
     # xArr, yArr = loadDataSet('ex0.txt')
     # ws = standRegres(xArr, yArr)
@@ -163,13 +202,16 @@ if __name__ == "__main__":
     # ax.plot(ridgeWeights)
     # plt.show()
 
-    xArr, yArr = loadDataSet("abalone.txt")
+    # xArr, yArr = loadDataSet("abalone.txt")
     # print stageWise(xArr, yArr, 0.01, 200)
     # print stageWise(xArr, yArr, 0.001, 5000)
-    xMat = mat(xArr)
-    yMat = mat(yArr).T
-    xMat = regularize(xMat)
-    yM = mean(yMat, 0)
-    yMat = yMat - yM
-    weights = standRegres(xMat, yMat.T)
-    print weights.T
+    # xMat = mat(xArr)
+    # yMat = mat(yArr).T
+    # xMat = regularize(xMat)
+    # yM = mean(yMat, 0)
+    # yMat = yMat - yM
+    # weights = standRegres(xMat, yMat.T)
+    # print weights.T
+
+    lgX, lgY = [], []
+    setDataCollect(lgX, lgY)
