a69c22eff871517c91e8fded226528ce19bef004
raxxar1024 <yjw013809@gmail.com>
2018-05-31 09:07:50 +0800

    regTrees

--- 9/regTrees.py
+++ 9/regTrees.py
@@ -33,6 +33,43 @@ def binSplitDataSet(dataSet, feature, value):
 #     retTree['right'] = createTree(rSet, leafType, errType, ops)
 #     return retTree
 
+def regLeaf(dataSet):
+    return mean(dataSet[:, -1])
+
+
+def regErr(dataSet):
+    return var(dataSet[:, -1]) * shape(dataSet)[0]
+
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    tolS = ops[0]
+    tolN = ops[1]
+    # （以下两行）如果所有值相等则退出
+    if len(set(dataSet[:, -1].T.tolist()[0])) == 1:
+        return None, leafType(dataSet)
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS = inf
+    bestIndex = 0
+    bestValue = 0
+    for featIndex in range(n - 1):
+        for splitVal in set(dataSet[:, featIndex]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if shape(mat0)[0] < tolN or shape(mat1)[0] < tolN:
+                continue
+            newS = errType(mat0) + errType(mat1)
+            if newS < bestS:
+                bestIndex = featIndex
+                bestValue = splitVal
+                bestS = newS
+    # （以下两行）如果误差减少不大则退出
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, bestIndex, bestValue)
+    if shape(mat0)[0] < tolN or shape(mat1)[0] < tolN:
+        # （以下两行）如果切分出的数据集很小则退出
+        return None, leafType(dataSet)
+    return bestIndex, bestValue
 
 
 if __name__ == "__main__":
