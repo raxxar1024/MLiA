299c92872702a57a4b9a8777db73676dfa7201d6
raxxar1024 <yjw013809@gmail.com>
2018-06-01 14:38:24 +0800

    regTrees

--- 9/regTrees.py
+++ 9/regTrees.py
@@ -15,23 +15,10 @@ def loadDataSet(fileName):
 
 
 def binSplitDataSet(dataSet, feature, value):
-    mat0 = dataSet[nonzero(dataSet[:, feature] > value)[0], :][0]
-    mat1 = dataSet[nonzero(dataSet[:, feature] <= value)[0], :][0]
+    mat0 = dataSet[nonzero(dataSet[:, feature] > value)[0], :]
+    mat1 = dataSet[nonzero(dataSet[:, feature] <= value)[0], :]
     return mat0, mat1
 
-
-# def createTree(dataSet, leafType=regLeaf, errType=regErr. ops=(1,4)):
-#     feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
-#     # 满足停止条件时返回叶节点值
-#     if feat == None:
-#         return val
-#     retTree = {}
-#     retTree['spInd'] = feat
-#     retTree['spVal'] = val
-#     lSet, rSet = binSplitDataSet(dataSet, feat, val)
-#     retTree['left'] = createTree(lSet, leafType, errType, ops)
-#     retTree['right'] = createTree(rSet, leafType, errType, ops)
-#     return retTree
 
 def regLeaf(dataSet):
     return mean(dataSet[:, -1])
@@ -53,7 +40,7 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
     bestIndex = 0
     bestValue = 0
     for featIndex in range(n - 1):
-        for splitVal in set(dataSet[:, featIndex]):
+        for splitVal in set(dataSet[:, featIndex].T.tolist()[0]):
             mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
             if shape(mat0)[0] < tolN or shape(mat1)[0] < tolN:
                 continue
@@ -72,6 +59,24 @@ def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
     return bestIndex, bestValue
 
 
+def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1, 4)):
+    feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
+    # 满足停止条件时返回叶节点值
+    if feat == None:
+        return val
+    retTree = {}
+    retTree['spInd'] = feat
+    retTree['spVal'] = val
+    lSet, rSet = binSplitDataSet(dataSet, feat, val)
+    retTree['left'] = createTree(lSet, leafType, errType, ops)
+    retTree['right'] = createTree(rSet, leafType, errType, ops)
+    return retTree
+
+
 if __name__ == "__main__":
-    testMat = mat(eye(4))
-    print binSplitDataSet(testMat, 1, 0.5)
+    # testMat = mat(eye(4))
+    # print binSplitDataSet(testMat, 1, 0.5)
+
+    myDat = loadDataSet('ex00.txt')
+    myMat = mat(myDat)
+    print createTree(myMat)
