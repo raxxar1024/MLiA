817259be16952bf9afde173703ad087dcbdaddee
yjw013809 <yjw013809@gmail.com>
2018-06-07 23:25:41 +0800

    treeExplore

---
+++ 9/treeExplore.py
@@ -0,0 +1,20 @@
+# !/usr/bin/python
+# -*- coding=utf-8 -*-
+from numpy import *
+from Tkinter import *
+import regTrees
+
+
+def reDraw(tolS, tolN):
+    pass
+
+
+def drawNewTree():
+    pass
+
+
+root = Tk()
+
+Label(root, text="Plot Place Holder").grid(row=0, columnspan=3)
+
+root.mainloop()
