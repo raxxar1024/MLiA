a54ed3bd414dfebf06ed0d4ef86a0cc5e5d25bee
yjw013809 <yjw013809@gmail.com>
2018-06-08 14:46:46 +0800

    treeExplore

--- 9/treeExplore.py
+++ 9/treeExplore.py
@@ -17,4 +17,22 @@ root = Tk()
 
 Label(root, text="Plot Place Holder").grid(row=0, columnspan=3)
 
+Label(root, text="tolN").grid(row=1, column=0)
+tolNentry = Entry(root)
+tolNentry.grid(row=1, column=1)
+tolNentry.insert(0, '10')
+Label(root, text='tolS').grid(row=2, column=0)
+tolSentry = Entry(root)
+tolSentry.grid(row=2, column=1)
+tolSentry.insert(0, '1.0')
+Button(root, text="ReDraw", command=drawNewTree).grid(row=1, column=2, rowspan=3)
+chkBtnVar = IntVar()
+chkBtn = Checkbutton(root, text="Model Tree", variable=chkBtnVar)
+chkBtn.grid(row=3, column=0, columnspan=2)
+
+reDraw.rawDat = mat(regTrees.loadDataSet('sine.txt'))
+reDraw.testDat = arange(min(reDraw.rawDat[:, 0]), max(reDraw.rawDat[:, 0]), 0.01)
+
+reDraw(1.0, 10)
+
 root.mainloop()
