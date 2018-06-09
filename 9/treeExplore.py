aefc07b99b3db9ce14c347871fc129d58f0e2a77
raxxar1024 <yjw013809@gmail.com>
2018-06-09 16:57:19 +0800

    treeExplore

--- 9/treeExplore.py
+++ 9/treeExplore.py
@@ -3,10 +3,28 @@
 from numpy import *
 from Tkinter import *
 import regTrees
+import matplotlib
+
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasAgg
+from matplotlib.figure import Figure
 
 
 def reDraw(tolS, tolN):
-    pass
+    reDraw.f.clf()
+    reDraw.a = reDraw.f.add_subplot(111)
+    if chkBtnVar.get():
+        # 检查复选框是否选中
+        if tolN < 2:
+            tolN = 2
+        myTree = regTrees.createTree(reDraw.rawDat, regTrees.modelLeaf, regTrees.modelErr, (tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat, regTrees.modelTreeEval)
+    else:
+        myTree = regTrees.createTree(reDraw.rawDat, ops=(tolS, tolN))
+        yHat = regTrees.createForeCast(myTree, reDraw.testDat)
+    reDraw.a.scatter(reDraw.rawDat[:, 0], reDraw.rawDat[:, 1], s=5)
+    reDraw.a.plot(reDraw.testDat, yHat, linewidth=2.0)
+    reDraw.canvas.show()
 
 
 def drawNewTree():
@@ -34,5 +52,10 @@ reDraw.rawDat = mat(regTrees.loadDataSet('sine.txt'))
 reDraw.testDat = arange(min(reDraw.rawDat[:, 0]), max(reDraw.rawDat[:, 0]), 0.01)
 
 reDraw(1.0, 10)
+
+reDraw.f = Figure(figsize=(5, 4), dpi=100)
+reDraw.canvas = FigureCanvasAgg(reDraw(reDraw.f, master=root))
+reDraw.canvas.show()
+reDraw.canvas.get_tk_widget().grid(row=0, columnspan=3)
 
 root.mainloop()
