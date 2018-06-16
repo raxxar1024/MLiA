62f2d3c1b1f01705a2766a2f3bf5f2d5c12c1cae
yjw013809 <yjw013809@gmail.com>
2018-06-16 23:21:16 +0800

    kMeans

--- 10/kMeans.py
+++ 10/kMeans.py
@@ -17,7 +17,7 @@ def distEclud(vecA, vecB):
     return sqrt(sum(power(vecA - vecB, 2)))
 
 
-def ranCent(dataSet, k):
+def randCent(dataSet, k):
     n = shape(dataSet)[1]
     centroids = mat(zeros((k, n)))
     # 构建簇质心
@@ -28,11 +28,39 @@ def ranCent(dataSet, k):
     return centroids
 
 
+def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m, 2)))
+    centroids = createCent(dataSet, k)
+    clusterChanged = True
+    while clusterChanged:
+        clusterChanged = False
+        for i in range(m):
+            minDist = inf
+            minIndex = -1
+            for j in range(k):
+                # （以下三行）寻找最近的质心
+                distJI = distMeas(centroids[j, :], dataSet[i, :])
+                if distJI < minDist:
+                    minDist = distJI
+                    minIndex = j
+            if clusterAssment[i, 0] != minIndex:
+                clusterChanged = True
+            clusterAssment[i, :] = minIndex, minDist ** 2
+        # （以下四行）更新质心的位置
+        print centroids
+        for cent in range(k):
+            ptsInClust = dataSet[nonzero(clusterAssment[:, 0].A == cent)[0]]
+            centroids[cent, :] = mean(ptsInClust, axis=0)
+    return centroids, clusterAssment
+
+
 if __name__ == "__main__":
     datMat = mat(loadDataSet('testSet.txt'))
     print min(datMat[:, 0])
     print min(datMat[:, 1])
     print max(datMat[:, 0])
     print max(datMat[:, 1])
-    print ranCent(datMat, 2)
+    print randCent(datMat, 2)
     print distEclud(datMat[0], datMat[1])
+    print kMeans(datMat, 4)
