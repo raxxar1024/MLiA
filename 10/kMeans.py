7dd14305276251e7b84ff6d937a33ae7c3a503b1
yjw013809 <yjw013809@gmail.com>
2018-06-18 23:05:36 +0800

    kMeans

--- 10/kMeans.py
+++ 10/kMeans.py
@@ -55,6 +55,39 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
     return centroids, clusterAssment
 
 
+def biKmeans(dataSet, k, distMeas=distEclud):
+    m = shape(dataSet)[0]
+    clusterAssment = mat(zeros((m, 2)))
+    # (以下两行)创建一个初始簇
+    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centList = [centroid0]
+    for j in range(m):
+        clusterAssment[j, 1] = distMeas(mat(centroid0), dataSet[j, :]) ** 2
+    while len(centList) < k:
+        lowestSSE = inf
+        for i in range(len(centList)):
+            # (以下两行)尝试划分每一簇
+            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:, 0].A == i)[0], :]
+            centroidMat, splitClusterAss = kMeans(ptsInCurrCluster, 2, distMeas)
+            sseSplit = sum(splitClusterAss[:, 1])
+            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            print "sseSplit, and notSplit: ", sseSplit, sseNotSplit
+            if sseSplit + sseNotSplit < lowestSSE:
+                bestCentToSplit = i
+                bestNewCents = centroidMat
+                bestClustAss = splitClusterAss.copy()
+                lowestSSE = sseSplit + sseNotSplit
+        # (以下两行)更新簇的分配结果
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        print "the bestCentToSplit is: ", bestCentToSplit
+        print "the len of bestClusterAss is: ", len(bestClustAss)
+        centList[bestCentToSplit] = bestNewCents[0, :]
+        centList.append(bestNewCents[1, :])
+        clusterAssment[nonzero(clusterAssment[:, 0]).A == bestCentToSplit[0], :] = bestClustAss
+    return mat(centList), clusterAssment
+
+
 if __name__ == "__main__":
     datMat = mat(loadDataSet('testSet.txt'))
     # print min(datMat[:, 0])
