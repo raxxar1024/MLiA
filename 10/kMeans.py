b45f1b99e852a43ad93bacdda86c1fd028fd5171
yjw013809 <yjw013809@gmail.com>
2018-06-24 17:31:47 +0800

    kMeans

--- 10/kMeans.py
+++ 10/kMeans.py
@@ -127,6 +127,40 @@ def massPlaceFind(fileName):
     fw.close()
 
 
+def distSLC(vecA, vecB):
+    a = sin(vecA[0, 1] * pi / 180) * sin(vecB[0, 1] * pi / 180)
+    b = cos(vecA[0, 1] * pi / 180) * cos(vecB[0, 1] * pi / 180) * cos(pi * (vecB[0, 0] - vecA[0, 0]) / 180)
+    return arccos(a + b) * 6371.0
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def clusterClubs(numClust=5):
+    datList = []
+    for line in open('places.txt').readlines():
+        lineArr = line.split('\t')
+        datList.append([float(lineArr[4]), float(lineArr[3])])
+    datMat = mat(datList)
+    myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
+    fig = plt.figure()
+    rect = [0.1, 0.1, 0.8, 0.8]
+    scatterMarkers = ['s', 'o', '^', '8', 'p', 'd', 'v', 'h', '>', '<']
+    axprops = dict(xticks=[], yticks=[])
+    ax0 = fig.add_axes(rect, label='ax0', **axprops)
+    imgP = plt.imread('Portland.png')
+    ax0.imshow(imgP)
+    ax1 = fig.add_axes(rect, label='ax1', frameon=False)
+    for i in range(numClust):
+        ptsInCurrCluster = datMat[nonzero(clustAssing[:, 0].A == i)[0], :]
+        markerStyle = scatterMarkers[i % len(scatterMarkers)]
+        ax1.scatter(ptsInCurrCluster[:, 0].flatten().A[0], ptsInCurrCluster[:, 1].flatten().A[0], marker=markerStyle,
+                    s=90)
+    ax1.scatter(myCentroids[:, 0].flatten().A[0], myCentroids[:, 1].flatten().A[0], marker='+', s=300)
+    plt.show()
+
+
 if __name__ == "__main__":
     # datMat = mat(loadDataSet('testSet.txt'))
     # print min(datMat[:, 0])
@@ -141,5 +175,11 @@ if __name__ == "__main__":
     # centList, myNewAssments = biKmeans(dataMat3, 3)
     # print centList
 
-    print geoGrab('1 VA Center', 'Augusta, ME')
-    massPlaceFind('portlandClubs.txt')  # yahoo服务不能用，但反正是用来生成了places.txt文件
+    # print geoGrab('1 VA Center', 'Augusta, ME')
+    # massPlaceFind('portlandClubs.txt')  # yahoo服务不能用，但反正是用来生成了places.txt文件
+
+    clusterClubs(5)
+
+
+
+
