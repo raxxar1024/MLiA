3848bf554f5c479a92769ec4cc32ab52fb21d65c
raxxar1024 <yjw013809@gmail.com>
2018-06-25 14:22:42 +0800

    apriori

---
+++ 11/apriori.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python
+# -*- coding=utf-8 -*-
+
+
+def loadDataSet():
+    return [[1, 3, 4], [2, 3, 5], [1, 2, 3, 5], [2, 5]]
+
+
+if __name__ == "__main__":
+    datSet = loadDataSet()
