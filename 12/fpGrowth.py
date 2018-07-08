f970c4cd30cc547a5d1afe89d830c343ab4b8724
yjw013809 <yjw013809@gmail.com>
2018-07-08 22:57:19 +0800

    fpGrowth

--- 12/fpGrowth.py
+++ 12/fpGrowth.py
@@ -18,6 +18,50 @@ class treeNode:
             child.disp(ind + 1)
 
 
+def createTree(dateSet, minSup=1):
+    headerTable = {}
+    for trans in dateSet:
+        for item in trans:
+            headerTable[item] = headerTable.get(item, 0) + dateSet[trans]
+        for k in headerTable.keys():
+            if headerTable[k] < minSup:
+                del (headerTable[k])
+        freqitemSet = set(headerTable.keys())
+        if len(freqitemSet) == 0:
+            return None, None
+        for k in headerTable:
+            headerTable[k] = [headerTable[k], None]
+        retTree = treeNode('Null Set', 1, None)
+        for tranSet, count in dateSet.items():
+            localD = {}
+            for item in tranSet:
+                if item in freqitemSet:
+                    localD[item] = headerTable[item]
+            if len(localD) > 0:
+                orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
+                updateTree(orderedItems, retTree, headerTable, count)
+    return retTree, headerTable
+
+
+def updateTree(items, inTree, headerTable, count):
+    if items[0] in inTree.children:
+        inTree.children[items[0]].inc(count)
+    else:
+        inTree.children[items[0]] = treeNode()
+        if headerTable[item[0]][1] == None:
+            headerTable[items][1] = inTree
+        else:
+            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+    if len(items) > 1:
+        updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+
+
+def updateHeader(nodeToTest, targetNode):
+    while nodeToTest.nodeLink != None:
+        nodeToTest = nodeToTest.nodeLink
+    nodeToTest.nodeLink = targetNode
+
+
 if __name__ == "__main__":
     rootNode = treeNode('pyramid', 9, None)
     rootNode.children['eye'] = treeNode('eye', 13, None)
