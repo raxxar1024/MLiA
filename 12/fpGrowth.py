6c3f61a537e611a35b78fd6fc9c273827668c816
raxxar1024 <yjw013809@gmail.com>
2018-07-09 09:55:44 +0800

    fpGrowth

--- 12/fpGrowth.py
+++ 12/fpGrowth.py
@@ -18,28 +18,28 @@ class treeNode:
             child.disp(ind + 1)
 
 
-def createTree(dateSet, minSup=1):
+def createTree(dataSet, minSup=1):
     headerTable = {}
-    for trans in dateSet:
+    for trans in dataSet:
         for item in trans:
-            headerTable[item] = headerTable.get(item, 0) + dateSet[trans]
-        for k in headerTable.keys():
-            if headerTable[k] < minSup:
-                del (headerTable[k])
-        freqitemSet = set(headerTable.keys())
-        if len(freqitemSet) == 0:
-            return None, None
-        for k in headerTable:
-            headerTable[k] = [headerTable[k], None]
-        retTree = treeNode('Null Set', 1, None)
-        for tranSet, count in dateSet.items():
-            localD = {}
-            for item in tranSet:
-                if item in freqitemSet:
-                    localD[item] = headerTable[item]
-            if len(localD) > 0:
-                orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
-                updateTree(orderedItems, retTree, headerTable, count)
+            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+    for k in headerTable.keys():
+        if headerTable[k] < minSup:
+            del (headerTable[k])
+    freqitemSet = set(headerTable.keys())
+    if len(freqitemSet) == 0:
+        return None, None
+    for k in headerTable:
+        headerTable[k] = [headerTable[k], None]
+    retTree = treeNode('Null Set', 1, None)
+    for tranSet, count in dataSet.items():
+        localD = {}
+        for item in tranSet:
+            if item in freqitemSet:
+                localD[item] = headerTable[item][0]
+        if len(localD) > 0:
+            orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
+            updateTree(orderedItems, retTree, headerTable, count)
     return retTree, headerTable
 
 
@@ -47,9 +47,9 @@ def updateTree(items, inTree, headerTable, count):
     if items[0] in inTree.children:
         inTree.children[items[0]].inc(count)
     else:
-        inTree.children[items[0]] = treeNode()
-        if headerTable[item[0]][1] == None:
-            headerTable[items][1] = inTree
+        inTree.children[items[0]] = treeNode(items[0], count, inTree)
+        if headerTable[items[0]][1] == None:
+            headerTable[items][0][1] = inTree.children[items[0]]
         else:
             updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
     if len(items) > 1:
