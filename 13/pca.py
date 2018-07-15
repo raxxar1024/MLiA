ae7b3e9729a40f316f6d71f1aad64f55e60499b5
yjw013809 <yjw013809@gmail.com>
2018-07-15 10:12:54 +0800

    pca

--- 13/pca.py
+++ 13/pca.py
@@ -11,5 +11,21 @@ def loadDataSet(fileName, delim='\t'):
     return mat(datArr)
 
 
+def pca(dataMat, topNfeat=9999999):
+    meanVals = mean(dataMat, axis=0)
+    # 取平均值
+    meanRemoved = dataMat - meanVals
+    covMat = cov(meanRemoved, rowvar=0)
+    eigVals, eigVects = linalg.eig(mat(covMat))
+    eigValInd = argsort(eigVals)
+    # 从小到大对N个值排序
+    eigValInd = eigValInd[:-(topNfeat + 1):-1]
+    redEigVects = eigVects[:, eigValInd]
+    # 数据转换到新空间
+    lowDDataMat = meanRemoved * redEigVects
+    reconMat = (lowDDataMat * redEigVects.T) + meanVals
+    return lowDDataMat, reconMat
+
+
 if __name__ == "__main__":
     dataMat = loadDataSet('testSet.txt')
