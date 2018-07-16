e9b14248a6b82ec0506660ba7d723311697fc993
yjw013809 <yjw013809@gmail.com>
2018-07-16 14:07:56 +0800

    pca

--- 13/pca.py
+++ 13/pca.py
@@ -32,10 +32,12 @@ if __name__ == "__main__":
     lowDMat, reconMat = pca(dataMat, 1)
     # lowDMat, reconMat = pca(dataMat, 2)
     print shape(lowDMat)
-    import matplotlib
+
+    # import matplotlib
     import matplotlib.pyplot as plt
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
     ax.scatter(dataMat[:, 0].flatten().A[0], dataMat[:, 1].flatten().A[0], marker='^', s=90)
     ax.scatter(reconMat[:, 0].flatten().A[0], reconMat[:, 1].flatten().A[0], marker='o', s=50, c='red')
+    plt.show()
