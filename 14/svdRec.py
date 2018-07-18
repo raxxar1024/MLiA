678b89574b12565fa15cdadb048b9a9504b4d876
yjw013809 <yjw013809@gmail.com>
2018-07-18 15:50:27 +0800

    svd

---
+++ 14/svdRec.py
@@ -0,0 +1,22 @@
+# !/usr/bin/python
+# -*- coding=utf-8 -*-
+
+from numpy import *
+
+
+def loadExData():
+    return [
+        [1, 1, 1, 0, 0],
+        [2, 2, 2, 0, 0],
+        [1, 1, 1, 0, 0],
+        [5, 5, 5, 0, 0],
+        [1, 1, 0, 2, 2],
+        [0, 0, 0, 3, 3],
+        [0, 0, 0, 1, 1],
+    ]
+
+
+if __name__ == "__main__":
+    Data = loadExData()
+    U, Sigma, VT = linalg.svd(Data)
+    print Sigma
