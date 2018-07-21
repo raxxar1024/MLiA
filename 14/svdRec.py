f46602990f64f8d78cef38d7ebadd8605ed75c89
raxxar1024 <yjw013809@gmail.com>
2018-07-21 10:02:39 +0800

    svd

--- 14/svdRec.py
+++ 14/svdRec.py
@@ -2,6 +2,7 @@
 # -*- coding=utf-8 -*-
 
 from numpy import *
+from numpy import linalg as la
 
 
 def loadExData():
@@ -16,10 +17,33 @@ def loadExData():
     ]
 
 
-if __name__ == "__main__":
-    Data = loadExData()
-    U, Sigma, VT = linalg.svd(Data)
-    print Sigma
+def euclidSim(inA, inB):
+    return 1.0 / (1.0 + la.norm(inA - inB))
 
-    Sig3 = mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
-    print U[:, : 3] * Sig3 * VT[: 3, :]
+
+def pearsSim(inA, inB):
+    if len(inA) < 3:
+        return 1.0
+    return 0.5 + 0.5 * corrcoef(inA, inB, rowvar=0)[0][1]
+
+
+def cosSim(inA, inB):
+    num = float(inA.T * inB)
+    denom = la.norm(inA) * la.norm(inB)
+    return 0.5 + 0.5 * (num / denom)
+
+
+if __name__ == "__main__":
+    # Data = loadExData()
+    # U, Sigma, VT = linalg.svd(Data)
+    # print Sigma
+    # Sig3 = mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
+    # print U[:, : 3] * Sig3 * VT[: 3, :]
+
+    myMat = mat(loadExData())
+    print euclidSim(myMat[:, 0], myMat[:, 4])
+    print euclidSim(myMat[:, 0], myMat[:, 0])
+    print cosSim(myMat[:, 0], myMat[:, 4])
+    print cosSim(myMat[:, 0], myMat[:, 0])
+    print pearsSim(myMat[:, 0], myMat[:, 4])
+    print pearsSim(myMat[:, 0], myMat[:, 0])
