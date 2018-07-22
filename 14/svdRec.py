606749d4967ec9ca224143abb4d57cd111789277
yjw013809 <yjw013809@gmail.com>
2018-07-22 18:45:28 +0800

    svd

--- 14/svdRec.py
+++ 14/svdRec.py
@@ -7,17 +7,17 @@ from numpy import linalg as la
 
 def loadExData():
     return [
-        [1, 1, 1, 0, 0],
-        [2, 2, 2, 0, 0],
-        [1, 1, 1, 0, 0],
-        [5, 5, 5, 0, 0],
-        [1, 1, 0, 2, 2],
+        [0, 0, 0, 2, 2],
         [0, 0, 0, 3, 3],
         [0, 0, 0, 1, 1],
+        [1, 1, 1, 0, 0],
+        [2, 2, 2, 0, 0],
+        [5, 5, 5, 0, 0],
+        [1, 1, 1, 0, 0]
     ]
 
 
-def euclidSim(inA, inB):
+def ecludSim(inA, inB):
     return 1.0 / (1.0 + la.norm(inA - inB))
 
 
@@ -33,6 +33,39 @@ def cosSim(inA, inB):
     return 0.5 + 0.5 * (num / denom)
 
 
+def standEst(dataMat, user, simMeas, item):
+    n = shape(dataMat)[1]
+    simTotal = 0.0
+    ratSimTotal = 0.0
+    for j in range(n):
+        userRating = dataMat[user, j]
+        if userRating == 0:
+            continue
+        overlap = nonzero(logical_and(dataMat[:, item].A > 0, dataMat[:, j].A > 0))[0]
+        if len(overlap) == 0:
+            similarity = 0
+        else:
+            similarity = simMeas(dataMat[overlap, item], dataMat[overlap, j])
+        # print 'the %d and %d similarty is: %f' % (item, j, similarity)
+        simTotal += similarity
+        ratSimTotal += similarity * userRating
+    if simTotal == 0:
+        return 0
+    else:
+        ratSimTotal / simTotal
+
+
+def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
+    unratedItems = nonzero(dataMat[user, :].A == 0)[1]
+    if len(unratedItems) == 0:
+        return 'you rated everything'
+    itemScores = []
+    for item in unratedItems:
+        estimatedScore = estMethod(dataMat, user, simMeas, item)
+        itemScores.append((item, estimatedScore))
+    return sorted(itemScores, key=lambda jj: jj[1], reverse=True)[:N]
+
+
 if __name__ == "__main__":
     # Data = loadExData()
     # U, Sigma, VT = linalg.svd(Data)
@@ -40,10 +73,18 @@ if __name__ == "__main__":
     # Sig3 = mat([[Sigma[0], 0, 0], [0, Sigma[1], 0], [0, 0, Sigma[2]]])
     # print U[:, : 3] * Sig3 * VT[: 3, :]
 
+    # myMat = mat(loadExData())
+    # print euclidSim(myMat[:, 0], myMat[:, 4])
+    # print euclidSim(myMat[:, 0], myMat[:, 0])
+    # print cosSim(myMat[:, 0], myMat[:, 4])
+    # print cosSim(myMat[:, 0], myMat[:, 0])
+    # print pearsSim(myMat[:, 0], myMat[:, 4])
+    # print pearsSim(myMat[:, 0], myMat[:, 0])
+
     myMat = mat(loadExData())
-    print euclidSim(myMat[:, 0], myMat[:, 4])
-    print euclidSim(myMat[:, 0], myMat[:, 0])
-    print cosSim(myMat[:, 0], myMat[:, 4])
-    print cosSim(myMat[:, 0], myMat[:, 0])
-    print pearsSim(myMat[:, 0], myMat[:, 4])
-    print pearsSim(myMat[:, 0], myMat[:, 0])
+    myMat[0, 1] = myMat[0, 0] = myMat[1, 0] = myMat[2, 0] = 4
+    myMat[3, 3] = 2
+    print myMat
+    print recommend(myMat, 2)
+    print recommend(myMat, 2, simMeas=ecludSim)
+    print recommend(myMat, 2, simMeas=pearsSim)
